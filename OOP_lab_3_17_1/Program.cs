d20d83ebbe233e533fd89f7b7707dca6ac47b9b4
Роман Данилевич <dwordchanel@gmail.com>
2020-04-30 15:24:16 +0300

    Add project files.

---
+++ OOP_lab_3_17_1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace OOP_lab_3_17_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            int round = 0;
+            int square = 0;
+
+            for (int i = 1; i < s.Length; ++i)
+            {
+                switch (s[i])
+                {
+                    case '(':
+                        ++round;
+                        break;
+
+                    case ')':
+                        --round;
+                        break;
+
+                    case '[':
+                        ++square;
+                        break;
+
+                    case ']':
+                        --square;
+                        break;
+                }
+            }
+
+            for (int i = 1; i < s.Length; ++i)
+            {
+                if ((s[i] == '(') && (s[i + 1] != ')'))
+                {
+                    s = s.Remove(i + 1, 1);
+                    --i;
+                }
+            }
+
+            Console.WriteLine(s);
+
+            if ((round == 0) && (square == 0))
+            {
+                Console.WriteLine("Кiлькiсть вiдкритих та закритих дужок спiвпадає.");
+            }
+            else
+            {
+                Console.WriteLine("Кiлькiсть вiдкритих та закритих дужок не спiвпадає.");
+            }
+        }
+    }
+}
